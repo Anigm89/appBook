d190b5d75423b4e265ca3d0101a272885e3a0956
Aniana <anigm89@gmail.com>
2024-04-29 13:49:24 +0200

    buscador titulo mejor

--- front/src/components/BuscadorTitulo.jsx
+++ front/src/components/BuscadorTitulo.jsx
@@ -1,12 +1,12 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef} from "react";
 import { LibrosContext } from '../hooks/LibrosContext';
 
-function BuscadorTitulo({onSearch, mostrarResultados }){
+function BuscadorTitulo({onSearch, mostrarResultados, handleReset }){
 
     const [titulo, setTitulo] = useState('');
     const [resultados, setResultados ] = useState([]);
     const { Buscartitulo } = useContext(LibrosContext);
-    const [ mensaje, setMensaje] = useState(false)
+    const [ mensaje, setMensaje] = useState(false);
 
 
     const handleSearch = async (e) => {
@@ -21,20 +21,18 @@ function BuscadorTitulo({onSearch, mostrarResultados }){
             setMostrarMensaje(true); 
         }
     }
-
-    
-
     return (
         <>
         <form >
           <input type="text" placeholder="Busca por titulo"  value={titulo} onChange={e => setTitulo(e.target.value)} required />
-          <button onClick={handleSearch}>Buscar</button>
+          <button onClick={handleSearch} >Buscar</button>
         </form>
        { mostrarResultados  &&  resultados.length > 0 ?
             (<ul>
                 {resultados[0].map((res, i) => (
                     <li key={i}><img src={res.imagen} alt={res.titulo} /> </li>
                 ))}
+                <button onClick={handleReset}>Reset</button>
             </ul>)
             : null
        }
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { LibrosContext } from '../hooks/LibrosContext'
 import { useContext, useState, useEffect } from "react";
 import BuscadorTitulo from "../components/BuscadorTitulo";
+import { useRef } from "react";
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
   const { fetchData } = useContext(LibrosContext);
   const [books, setBooks] = useState([]);
   const [buscados, setBuscados]  = useState([]);
+  const divRef = useRef(null)
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -22,36 +24,26 @@ const Home = () => {
         }
     };
     fetchBooks();
+    
   }, []);
 
   const onSearch = async (searchResults) => {
     setBuscados(searchResults);
+    divRef.current.style.display = 'none'
   };
   const handleReset = () => {
     setBuscados([]);
+    divRef.current.style.display = 'block'
+
   };
 
   return (
     <>
-      <BuscadorTitulo onSearch={onSearch} mostrarResultados={buscados.length > 0} />
+      <BuscadorTitulo onSearch={onSearch} mostrarResultados={buscados.length > 0} handleReset={handleReset} />
       <h2>Lista de libros</h2>
-     
+     <div className="todos" ref={divRef}>
       <ul className="home">
-      {buscados.length > 0 ? (
-          buscados.map(item => (
-            <li key={item.id}>
-              <Link to={`/${item.id}`}>
-                <div className="card">
-                  <h2>{item.titulo}</h2>
-                  <p>{item.autor}</p>
-                  <img src={item.imagen} alt={item.titulo} />
-                </div>
-              </Link>
-              <button onClick={handleReset}>Reset</button>
-            </li>
-          ))
-        ) : (
-          books.map(item => (
+      { books.map(item => (
             <li key={item.id}>
               <Link to={`/${item.id}`}>
                 <div className="card">
@@ -62,9 +54,11 @@ const Home = () => {
               </Link>
             </li>
           ))
-        )}
+      }
       </ul>
+      </div>
     </>
+    
   )
 };
 
