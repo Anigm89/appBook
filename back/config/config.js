099a2fcd13e9b412031a7ee1b6429cc3e476d244
Aniana <anigm89@gmail.com>
2024-04-30 17:00:19 +0200

    variables en.env

--- back/config/config.js
+++ back/config/config.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    port: 3306,
-    database: 'appbook'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_NAME || 'appbook'
 });
 console.log('Se ha conectado con éxito a la base de datos');
 
