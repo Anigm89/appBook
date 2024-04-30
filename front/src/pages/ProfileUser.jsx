153147a5d40a3aa68a394eaf26265b0d592c973e
Aniana <anigm89@gmail.com>
2024-04-30 17:04:45 +0200

    paginacion leidos

--- front/src/pages/ProfileUser.jsx
+++ front/src/pages/ProfileUser.jsx
@@ -2,11 +2,20 @@ import { useEffect ,useState, useContext} from "react";
 import LibrosPendientes from "../components/LibrosPendientes.jsx";
 import { Link } from "react-router-dom";
 import { LibrosContext } from '../hooks/LibrosContext';
+import ReactPaginate from 'react-paginate';
+
 
  function Profileuser({uid, token}){
 
     const { librosLeidos  } = useContext(LibrosContext); 
     const [leidos, setLeidos] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+    const resultsPerPage = 6; 
+    const pageCount = Math.ceil(leidos.length / resultsPerPage);
+    const offset = pageNumber * resultsPerPage;
+    const currentPageBooks = leidos.slice(offset, offset + resultsPerPage);
+    
+  
 
 
     useEffect(() => {
@@ -39,7 +48,7 @@ import { LibrosContext } from '../hooks/LibrosContext';
                 (
                     <ul>
                     {
-                        leidos.map((leido, index) =>(
+                        currentPageBooks.map((leido, index) =>(
                             <li key={index}>
                                <Link to={`/${leido.id_libro}`}>
                                
@@ -53,6 +62,17 @@ import { LibrosContext } from '../hooks/LibrosContext';
                 :
                 (<p>No has marcado ningún libro como leído</p>) }
             </div>
+            <ReactPaginate
+                previousLabel={'Anterior'}
+                nextLabel={'Siguiente'}
+                breakLabel={'...'}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={({ selected }) => setPageNumber(selected)}
+                containerClassName={'pagination'}
+                activeClassName={'active'}
+            />
             <div>
                 <h1>Libros que quiero leer</h1>
                 <LibrosPendientes uid={uid} actualizarLeidos={actualizarLeidos} token={token} />
