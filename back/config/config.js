885018c74703655b1400c8e8027303d494e6806e
Aniana <anigm89@gmail.com>
2024-04-30 13:36:49 +0200

    clean code

--- back/config/config.js
+++ back/config/config.js
@@ -13,24 +13,3 @@ console.log('Se ha conectado con éxito a la base de datos');
 
 module.exports = pool;
 
-
-/*
- async function db(){
-    try {
-        const db = await mysql.createConnection({
-            host     : 'localhost',
-            user     : 'root',
-            password : '',
-            port: 3306,
-            database: 'appbook'
-        });
-
-        console.log('Se ha conectado con éxito a la base de datos');
-        return db;
-    } catch (error) {
-        console.error('Error connecting to the database:', error.message);
-        throw error;
-    }
- } 
-module.exports = db;
-*/
--- back/controllers/productController.js
+++ back/controllers/productController.js
@@ -1,15 +1,14 @@
-//const db = require('../config/config');
 const pool = require('../config/config');
 
 const BookController = {
     async ShowBooks (req, res) {
         try{
-            //const connection = await db()
             const [books] = await pool.query('SELECT * FROM libros');
             res.json(books)
         }
         catch(error){
-            console.log(error)
+            res.status(500).json({ message: "Error al mostrar los libros" });
+
         }
     },
     async getById(req, res) {
@@ -18,7 +17,8 @@ const BookController = {
             const [bookId] = await pool.query(`SELECT * FROM libros where id = ${id}`);
             res.json(bookId[0])
         } catch (error) {
-            console.log(error)
+            res.status(500).json({ message: "Error al mostrar el libro" });
+
         }
     },
     async getAutor(req, res){
@@ -27,7 +27,7 @@ const BookController = {
             const [books] = await pool.query(`SELECT * FROM libros WHERE autor like '%${autor}%'`);
             res.json([books])
         } catch (error) {
-            console.log(error)
+            res.status(500).json({ message: "Error al mostrar los libros del autor" });
         }
     },
     async getGenero(req, res){
@@ -35,7 +35,7 @@ const BookController = {
             const [books] = await pool.query(`SELECT genero FROM libros`);
             res.json([books])
         } catch (error) {
-            console.log(error)
+            res.status(500).json({ message: "Error al buscar los géneros" });
         }
     },
     async getLibrosGenero(req, res){
@@ -45,7 +45,7 @@ const BookController = {
             const [books] = await pool.query(queryGenero);
             res.json([books])
         } catch (error) {
-            console.log(error)
+            res.status(500).json({ message: "Error al mostrar los libros por género" });
         }
     },
     async getTitle(req, res) {
@@ -54,7 +54,7 @@ const BookController = {
             const [titles] = await pool.query(`SELECT * FROM libros WHERE titulo like '%${title}%' OR subtitulo like '%${title}%'`);
             res.json([titles])
         } catch (error) {
-            console.log(error)
+            res.status(500).json({ message: "Error al buscar libros por titulo" });
         }
     },  
     async getKeywords(req, res) {
@@ -63,7 +63,7 @@ const BookController = {
             const [keywords] = await pool.query(`SELECT * FROM libros WHERE keywords like '%${keyword}%'`);
             res.json([keywords])
         } catch (error) {
-            console.log(error)
+            res.status(500).json({ message: "Error al buscar libros por palabras clave" });
         }
     },    
     async getRelacionados(req, res){
@@ -72,12 +72,8 @@ const BookController = {
 
             const searchQuery = `SELECT * FROM libros WHERE genero LIKE '%${genero}%' or autor LIKE '%${autor}%' or keywords like '%${keywords}%' LIMIT 5`
             const relacionados = await pool.query(searchQuery);
-            res.json(relacionados);
-            console.log(searchQuery)
-            console.log(relacionados)
-            
+            res.json(relacionados);            
         } catch (error) {
-            console.log(error);
             res.status(500).json({ message: "Error al buscar libros relacionados" });
         }
     },    
@@ -90,7 +86,6 @@ const BookController = {
             res.status(201).send(newBook)
         }
         catch(error){
-            console.log(error)
             res.status(500).json({ message: "Error creating book" });
         }
     },
@@ -107,7 +102,8 @@ const BookController = {
             res.status(201).json(updateBook)
         }
         catch(error){
-            console.log(error)
+            res.status(500).json({ message: "Error al actualizar" });
+
         }  
     },
     async deleteBook(req, res){
@@ -121,7 +117,7 @@ const BookController = {
         res.status(200).json({ mensaje: "Libro eliminado con éxito: ", deletebook })
         }
         catch(error){
-            console.log(error)
+            res.status(500).json({ message: "Error al eliminar el libro" });
         }          
     },
     async createUser (req, res){
@@ -130,15 +126,12 @@ const BookController = {
             const insertQuery = `INSERT INTO usuarios (email, uid) VALUES ("${email}", "${uid}")`;
             const newUser = await pool.query(insertQuery)
             if (!newUser) {
-                console.log('Error al insertar el email en la base de datos:', error);
                 res.status(500).send('Error al guardar el email en la base de datos');
             } else {
-                console.log('Email guardado en la base de datos:', email);
                 res.status(201).send('Email guardado correctamente');
             }
         }
         catch(error){
-            console.log(error)
             res.status(500).json({ message: "Error creating user" });
         }
     },
@@ -148,15 +141,12 @@ const BookController = {
             const insertQuery = `INSERT INTO leidos (id_libro, uid) VALUES ("${id_libro}", "${uid}")`;
             const leido = await pool.query(insertQuery)
             if (!leido) {
-                console.log('Error al insertar el libro en la tabla leidos:', error);
                 res.status(500).send('Error al guardar el libro como leido en la base de datos');
             } else {
-                console.log('libro guardado en la base de datos');
                 res.status(201).send('añadido como leido correctamente');
             }
         }
         catch(error){
-            console.log(error)
             res.status(500).json({ message: "Error al marcar como leido" });
         }
     },
@@ -169,6 +159,8 @@ const BookController = {
         }
         catch(error){
             console.log(error)
+            res.status(500).json({ message: "Error al actualizar" });
+
         }
     },
     async pendientes (req, res){
@@ -179,15 +171,12 @@ const BookController = {
                 const pendientes = await pool.query(insertQuery);
 
                 if (pendientes) {
-                    console.log('Libro guardado como pendiente en la base de datos', id_libro);
                     res.status(201).send('Libro añadido como pendiente correctamente');
                   } else {
-                    console.log('Error al insertar el libro en la tabla por leer:', result.error);
                     res.status(500).send('Error al guardar el libro como pendiente en la base de datos');
                   }
         }
         catch(error){
-            console.log(error)
             res.status(500).json({ message: "Error al marcar como pendiente" });
         }
     },
